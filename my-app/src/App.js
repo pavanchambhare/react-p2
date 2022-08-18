c79d724c420e78ba02c92f7e07dab1ff41094bd5
Pavan <pavanchambhare88@gmail.com>
2022-08-18 20:32:52 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <nav>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+    </nav>
+
+  <div className ="container">
+ <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ex quod dolorem adipisci, eum explicabo veniam iste officia, nulla itaque dolor, magnam nam quam ad! Mollitia nobis architecto harum perspiciatis.</p>
+  </div>
+  
+ </>
+  );
+}
+
+export default App;
